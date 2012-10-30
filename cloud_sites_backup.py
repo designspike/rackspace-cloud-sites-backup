0b132514749800a3bed98941ead4a50d583915ed
Will <will@designspike.com>
2012-10-29 18:17:19 -0700

    Make the rsync copy at the end actually work.

--- cloud_sites_backup.py
+++ cloud_sites_backup.py
@@ -56,7 +56,7 @@ for i, row in enumerate(list_feed.entry):
         print cmd
         subprocess.call(shlex.split(cmd))
 
-cmd = 'rsync -a '+backup_folder_path+'/cumulative/'
+cmd = 'rsync -a '+backup_base_path+'/ '+backup_folder_path+'/cumulative/'
 print cmd
 subprocess.call(shlex.split(cmd))
 
